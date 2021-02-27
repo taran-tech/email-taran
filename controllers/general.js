df5144c828acf21a68678d661ab5e681da29ca05
taran-tech <tdhanda@myseneca.ca>
2021-02-27 10:32:57 -0500

    final

--- controllers/general.js
+++ controllers/general.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 router.get("/", (req, res)=> {
     res.render("general/home", {
         title: "HOme page"
     })
 });
+
+router.get("/contact-us", (req, res)=>{
+    res.render("general/contactUs", {
+        title: "contact us"
+    })
+});
+router.post("/contact-us", (req, res) =>{
+    console.log(req.body);
+
+    let validationResults = {};
+    let passedValidation = true;
+    const{firstName, lastName, eamil, message}  = req.body;
+
+    if (typeof firstName !== "string" || firstName.length === 0 ){
+        validationResults.firstName = "you must specify a first name."
+        passedValidation = false;
+    }
+    else if (firstName.length < 2){
+        validationResults.firstName ="The first name must be at least 2 character";
+        passedValidation= false;
+    }
+
+    if(passedValidation){
+        res.send("suceess");
+    }
+    else{
+    res.render("general/contactUs", {
+        title: "contact us",
+        validationResults: validationResults,
+        values: req.body
+     })
+    };
+    
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 // Set up Handlebars
@@ -10,6 +10,9 @@ app.engine('.hbs', exphbs({
 }));
 
 app.set('view engine', '.hbs');
+
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //load controllers
 const generalController = require("./controllers/general");
